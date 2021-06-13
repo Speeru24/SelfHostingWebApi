451189747e0e4fc5e47543ecf5238bc3e8682ae5
shishir peeru <shishirpeeru@gmail.com>
2021-06-13 15:28:25 +0530

    Self Hosting Web Api application

---
+++ SelfHostingWebApi/SelfHostingConsole/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using SelfHostingConsole.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHostingConsole.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee {FirstName = "Shishir", LastName="Peeru", Address="Thakurli"},
+            new Employee {FirstName = "Shashank", LastName="Peeru", Address="Pune"},
+            new Employee {FirstName = "Sadanand", LastName="Peeru", Address="Bunder"},
+            new Employee {FirstName = "Seema", LastName="Peeru", Address="Keni"}
+        };
+
+        public IHttpActionResult GetAllEmployees()
+        {
+            return Ok(employees);
+        }
+    }
+}
---
+++ SelfHostingWebApi/SelfHostingConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace SelfHostingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOTE: Run Visual Studio as Administrator
+            
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute(
+                name:"API Default", 
+                routeTemplate:"api/{controller}/{id}",
+                defaults:null);
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                //Setting Formatters
+                var formatters = server.Configuration.Formatters;
+                //formatters.Add(formatters.JsonFormatter);
+                //formatters.Remove(formatters.XmlFormatter);
+
+                //formatters.Remove(formatters.JsonFormatter);
+                formatters.Add(formatters.XmlFormatter);
+
+                server.OpenAsync().Wait();
+                Console.WriteLine("Api application is  Up and Running at http://localhost:8080");
+
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
